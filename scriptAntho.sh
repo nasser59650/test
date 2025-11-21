8f9a4522f9b841714b6d8c41897a7ad7b37014a5
nasser59650 <nasser59650@gmail.com>
2025-11-21 09:01:50 +0100

    ok

--- scriptAntho.sh
+++ scriptAntho.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-<<<<<<< HEAD
 #Blague prof de scripting kfgjqddfsthjsdfgbstnvdsqqaalkfjdslsqdkfjsdlkjgqgejgfiqjgfqsdlkfgjq
-=======
-#Blague prof de scripting kfgjqdlsqdkfjsdlkjgqgejgfiqjgfqsdlkfgjq
->>>>>>> 5e0b645e618fe9ade0be86169e6275fcf58db35f
 #sudo rm -r /
 fulll testing
